fd99b53b2fc6945f307de1734c6ea36956c188d1
karanmehta567 <karanmehta29.km@gmail.com>
2026-02-13 22:11:23 +0530

    fixed route handling

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -1,6 +1,4 @@
 import { createAuthClient } from "better-auth/react"
 export const authClient = createAuthClient({
-    /** The base URL of the server (optional if you're using the same domain) */
-    baseURL: "https://jobflow-lemon.vercel.app/"
 })
 export const {signUp,signIn,useSession,signOut}=authClient
